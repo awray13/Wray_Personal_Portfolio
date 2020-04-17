b0ab6c541098f3b5997f14163e78a835c0b41c89
Ashton Wray <aricks1986@gmail.com>
2020-04-16 23:34:09 -0400

    updated index and main.js files to fix missing icon support

--- Wray_Personal_Portfolio/assets/js/main.js
+++ Wray_Personal_Portfolio/assets/js/main.js
@@ -56,12 +56,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="ri-menu-5-fill"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-toggle i').toggleClass('ri-menu-5-fill ri-close-circle-line');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -70,7 +70,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-toggle i').toggleClass('ri-menu-5-fill ri-close-circle-line');
           $('.mobile-nav-overly').fadeOut();
         }
       }
